ec418fb858e8bbf3bf0e804507ad7b8f92df3570
alectr0n4ik <andreyshka3@gmail.com>
2023-11-15 02:28:56 +0300

    upd

--- classes.py
+++ classes.py
@@ -2,24 +2,22 @@ import asyncio
 import datetime
 import json
 import time
-import statsmodels.api as sm
-import pandas as pd
-import websockets
-
-from sqlalchemy import create_engine, Column, Integer, String, Numeric, DateTime
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-from sqlalchemy.orm import declarative_base, Session
-from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
+
+import pandas as pd
+import statsmodels.api as sm
+import websockets
+from sqlalchemy import create_engine, Column, Integer, String, Numeric, \
+    DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 price_history = []
 
 Base = declarative_base()
 
-class FuturesTrade(Base):
 
+class FuturesTrade(Base):
     __tablename__ = 'futures_trades'
 
     id = Column(Integer, primary_key=True)
@@ -28,17 +26,19 @@ class FuturesTrade(Base):
     timestamp = Column(DateTime)
 
 
-
 class FuturesProcessor:
     def __init__(self, symbol):
         self.symbol = symbol
 
     @classmethod
     async def create_table(cls, table_name):
-        engine = create_engine('postgresql://postgres:12345@localhost:5432/postgres')
+        engine = create_engine(
+            'postgresql://postgres:12345@localhost:5432/postgres')
         if not engine.dialect.has_table(engine, table_name):
             Base.metadata.create_all(engine)
-            trade_entry = FuturesTrade(symbol=cls.symbol, price=0, timestamp=datetime.now(), table_name=table_name)
+            trade_entry = FuturesTrade(symbol=cls.symbol, price=0,
+                                       timestamp=datetime.now(),
+                                       table_name=table_name)
             session = cls.create_session(engine)
             session.add(trade_entry)
             session.commit()
@@ -49,17 +49,17 @@ class FuturesProcessor:
         Session = sessionmaker(bind=engine)
         return Session()
 
-
-
     async def handle_trade(self, data):
         try:
             if data:
                 data = json.loads(data)
-                print(f"Торговая пара ({self.symbol}): {data['s']}, Цена: {data['p']} {data['s']}")
+                print(
+                    f"Торговая пара ({self.symbol}): {data['s']}, Цена: {data['p']} {data['s']}")
                 trade_symbol = data['s']
                 trade_price = float(data['p'])
 
-                engine = create_engine('postgresql://postgres:12345@localhost:5432/postgres')
+                engine = create_engine(
+                    'postgresql://postgres:12345@localhost:5432/postgres')
                 session = self.create_session(engine)
 
                 # Вставка данных в таблицу
@@ -79,22 +79,25 @@ class FuturesProcessor:
             print(f"Произошла ошибка: {e}")
             await asyncio.sleep(10)
 
-
     async def delete_old_data(self):
-        engine = create_engine('postgresql://postgres:12345@localhost:5432/postgres')
+        engine = create_engine(
+            'postgresql://postgres:12345@localhost:5432/postgres')
         Session = sessionmaker(bind=engine)
         session = Session()
 
         # Удаление данных старше 1 часа для конкретной котировки
         session.query(FuturesTrade).filter(
-            FuturesTrade.timestamp < time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time() - 3600)),
+            FuturesTrade.timestamp < time.strftime('%Y-%m-%d %H:%M:%S',
+                                                   time.gmtime(
+                                                       time.time() - 3600)),
             FuturesTrade.symbol == self.symbol
         ).delete()
         session.commit()
         session.close()
 
     async def check_cointegration(self):
-        engine = create_engine('postgresql://postgres:12345@localhost:5432/postgres')
+        engine = create_engine(
+            'postgresql://postgres:12345@localhost:5432/postgres')
         Session = sessionmaker(bind=engine)
         session = Session()
 
@@ -105,10 +108,10 @@ class FuturesProcessor:
             #     print(i)
             # SQL-запрос для извлечения данных из базы данных
             futures_data = session.query(
-                FuturesTrade.timestamp, 
+                FuturesTrade.timestamp,
                 FuturesTrade.price
             ).filter(
-                FuturesTrade.symbol == 'BTCUSDT'#self.symbol
+                FuturesTrade.symbol == 'BTCUSDT'  # self.symbol
             ).order_by(
                 FuturesTrade.timestamp
             ).all()
@@ -119,10 +122,14 @@ class FuturesProcessor:
                 return
 
             # DataFrame из полученных данных
-            futures_df = pd.DataFrame(futures_data, columns=['timestamp', f'{self.symbol}_price']).set_index('timestamp')
+            futures_df = pd.DataFrame(futures_data, columns=['timestamp',
+                                                             f'{self.symbol}_price']).set_index(
+                'timestamp')
 
             # Расчет коинтеграции
-            result = sm.OLS(futures_df[f'{self.symbol}_price'], sm.add_constant(futures_df[f'{self.symbol}_price'])).fit()
+            result = sm.OLS(futures_df[f'{self.symbol}_price'],
+                            sm.add_constant(
+                                futures_df[f'{self.symbol}_price'])).fit()
 
             # Вывод результатов
             print(result.summary())
@@ -133,9 +140,8 @@ class FuturesProcessor:
         finally:
             session.close()
 
-
-
-    async def price_change_alert(data, symbol, price_change_threshold=0.01, time_frame=60):
+    async def price_change_alert(data, symbol, price_change_threshold=0.01,
+                                 time_frame=60):
         try:
             data = json.loads(data)
             current_price = data["p"]
@@ -144,13 +150,15 @@ class FuturesProcessor:
             price_history.append((current_price, timestamp))
 
             # Оставляем только данные, которые находятся в заданном временном диапазоне
-            while price_history and timestamp - price_history[0][1] > time_frame * 60:
+            while price_history and timestamp - price_history[0][
+                1] > time_frame * 60:
                 price_history.pop(0)
 
             # Рассчитываем изменение цены
             if len(price_history) >= 2:
                 previous_price = price_history[0][0]
-                percent_change = (current_price - previous_price) / previous_price
+                percent_change = (
+                                             current_price - previous_price) / previous_price
 
                 if abs(percent_change) >= price_change_threshold:
                     if percent_change > 0:
@@ -158,23 +166,21 @@ class FuturesProcessor:
                     else:
                         movement = "Цена падает"
 
-                    print(f"Изменение цены на {abs(percent_change) * 100:.2f}% за последние {time_frame} минут. {movement}")
+                    print(
+                        f"Изменение цены на {abs(percent_change) * 100:.2f}% за последние {time_frame} минут. {movement}")
 
         except Exception as e:
             print(f"Произошла ошибка при обработке данных: {e}")
-        
-            await asyncio.sleep(10)  # Проверяем каждые 10 секунд
-            
 
+            await asyncio.sleep(10)  # Проверяем каждые 10 секунд
 
     async def run(self, trade_class, engine):
-        async with websockets.connect(f"wss://stream.binance.com:9443/ws/{self.symbol}@trade") as ws:
+        async with websockets.connect(
+                f"wss://stream.binance.com:9443/ws/{self.symbol}@trade") as ws:
             while True:
                 response = await ws.recv()
-                
+
                 await self.handle_trade(response)
                 await self.delete_old_data()
                 await self.check_cointegration()
                 await self.price_change_alert(response, self.symbol, 'ethusdt')
-
-
